ed82ca12534a7114ab82760c6c6c6d8ae47d5195
Kolby <kolbykalafut@gmail.com>
2018-03-12 19:07:21 -0400

    Checkpoint 2 retrieve users: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,19 @@
 require 'httparty'
+require 'json'
 
 class Kele
 include HTTParty
+include JSON
 
   def initialize(email, password)
     response = self.class.post(base_api_url("sessions"), body: { email: email, password: password })
     raise "Invalid email or password" if response.code == 404
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(base_api_url("users/me"), headers: { authorization: @auth_token })
+    @user = JSON.parse(response.body)
   end
 
   private
