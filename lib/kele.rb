b283eed2a5c3f3c32054fa6e506b6e55fae97461
Kolby <kolbykalafut@gmail.com>
2018-03-12 21:20:46 -0400

    Checkpoint 3 mentor availability: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,6 +16,18 @@ include JSON
     @user = JSON.parse(response.body)
   end
 
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(base_api_url("mentors/#{mentor_id}/student_availability"), headers: { authorization: @auth_token })
+    @mentor_availability = JSON.parse(response.body)
+    available = []
+    @mentor_availability.each do |availability|
+      if availability["booked"] == nil
+        available << availability
+      end
+    end
+    available
+  end
+
   private
   def base_api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
